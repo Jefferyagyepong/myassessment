b1c750f6822969b35090e26b21adee4c9be88e8f
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-07-22 13:43:02 +0000

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -4,12 +4,12 @@ export default function Header() {
         <div>
             <header>
                 <h1>IFMA</h1>
-                <form>
+                
                     
            <input type="search" id="site-search" name="q" />
 
            <button>Search</button>
-                    </form>
+                
         
             </header>
         </div>
