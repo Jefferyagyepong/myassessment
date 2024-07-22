d7d7d555fe2a98f110d674bd35db54e253f9d7f2
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-07-22 13:48:31 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from "react";
 
 export default function Home() {
   const [movies, setMovies] = useState ([]);
-   const base_url = "https://image.tmdb.org/t/p/w500"
+   const base_url = "https://image.tmdb.org/t/p/w500";
   const getMovies = async () =>{
     try{
       await fetch ("https://api.themoviedb.org/3/discover/movie?api_key=906477201fdc7bb5edf0d0b6245069c5")
