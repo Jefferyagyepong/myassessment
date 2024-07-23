be080e048fe334fa23df48d13ef9e4733e23a917
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-07-23 11:04:09 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -38,9 +38,9 @@ useEffect(()=>{
       <main>
         <Header/>
         <div>
-          {movies.map((id)=>{
+          {movies.map((data)=>{
             return <>
-            <Image src={"https://image.tmdb.org/t/p/w780${id.backdrop_path}"} />
+            <Image src={"https://image.tmdb.org/t/p/w780${data.backdrop_path}"} />
             </>
             
           })}
